7fc6a66c7d5a302b0d9688f1adfc0314c1d2e882
Battsetseg Batsaikhan <battsetsegbatsaikhan@Battsetsegs-MacBook-Air.local>
2022-04-15 16:41:57 +0800

    practice Vuejs

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<script >
+import Table from './components/Table.vue';
+import Table1 from "./components/Table1.vue";
+import { useState } from './composables/state';
+
+export default {
+  components: { Table, Table1 },
+  // data(){
+  //   return {
+  //     num: 0
+  //   }
+  // },
+  // methods:{
+  //   setNum(num){
+  //     this.num = num;
+  //   }
+  // }
+  setup() {
+    const [state, setState] = useState(1);  
+    return {
+      state,
+      setState,
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="flex max-w-full h-screen">
+    <div class="h-screen bg-slate-600 w-1/5">
+      <div>
+        <div
+          class="bg-slate-800 h-14 text-white align-middle flex justify-center items-center"
+        >
+          <div class="flex-shrink-0 w-10 h-10 align-middle mr-4">
+            <img class="w-full h-full rounded-full" src="./logo.png" alt="" />
+          </div>
+          <h4>ГЭРЭГЭ СИСТЕМС ХХК</h4>
+        </div>
+        <div class="text-white cursor-pointer">
+          <div @click="setState(1)" class="m-8 hover:text-slate-800">Салбар, төхөөрөмж-1</div>
+          <div @click="setState(2)" class="m-8 hover:text-slate-800">Салбар, төхөөрөмж-2</div>
+        </div>
+      </div>
+    </div>
+    <div class="h-screen bg-slate-100 w-4/5">
+      <template v-if="state == 1">
+        <Table/>
+      </template>
+
+      <template v-else>
+        <Table1/>
+      </template>
+       
+    </div>
+  </div>
+</template>
+<style>
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Table1.vue
@@ -0,0 +1,62 @@
+<script>
+import { data2 } from "../data/data2";
+
+export default {
+  name: "Table1",
+  data() {
+    return {
+      boardFields: data2,
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="m-10">
+    <div class="flex justify-between mb-4">
+      <h4>Төхөөрөмжийн жагсаалт 2</h4>
+      <button class="bg-green-700 p-2 rounded-md text-white">Бүртгэх</button>
+    </div>
+    <div>
+      <div
+        class="flex justify-between mb-2 border-t border-slate-200 border-solid"
+      >
+        <p>Нийт 11</p>
+        <p>Xуудас 1/1> Мөрийн тоо: 11</p>
+      </div>
+      <table
+        class="table-fixed w-full border-slate-200 table table-striped table-bordered"
+      >
+        <thead>
+          <tr class="border-2 border-slate-200 border-solid">
+            <th class="w-24"></th>
+            <th>Сериал дугаа</th>
+            <th>Нэр</th>
+            <th>Тайлбар</th>
+            <th>Бүртгэгдсэн огноо</th>
+          </tr>
+        </thead>
+        <tbody class="divide-y-2 divide-slate-200 divide-solid">
+          <tr>
+            <td class="w-24"></td>
+            <td class="px-2"><input class="w-full" /></td>
+            <td class="px-2"><input class="w-full" /></td>
+            <td class="px-2"><input class="w-full" /></td>
+            <td class="px-2"></td>
+          </tr>
+        </tbody>
+        <tbody class="divide-y-2 divide-slate-200 divide-solid">
+          <tr v-for="item in boardFields" :key="item.id">
+            <td class="w-24 pl-8">
+              <img class="w-5 h-5" src="../delete.webp" alt="" />
+            </td>
+            <td>{{ item.serialNum }}</td>
+            <td>{{ item.name }}</td>
+            <td>{{ item.detail }}</td>
+            <td>{{ item.date }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
